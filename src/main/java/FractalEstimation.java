a16e80d6852f298f22a67dae1fb7f53a3614b5b2
Mikhail <mikhail-turicyn@yandex.ru>
2018-02-20 00:11:54 +0300

    some additions

--- src/main/java/FractalEstimation.java
+++ src/main/java/FractalEstimation.java
@@ -1,63 +1,85 @@
-public final class FractalEstimation {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FractalEstimation {
+    private double epsilon;
+    private double[] data;
+    int splitCnt;
 
     private FractalEstimation(){}
 
-    private static double rMetric(double rCur, double rPrev, double size){
+    public FractalEstimation(double[] data, int splitCnt){
+        this.data = data;
+        this.splitCnt = splitCnt;
+        this.epsilon = (getMaximum()-getMinimum())/splitCnt;
+    }
+
+    public FractalEstimation(double[] data, double epsilon){
+        this.data = data;
+        this.epsilon = epsilon;
+        this.splitCnt = (int)((getMaximum()-getMinimum())/epsilon);
+    }
+
+    private double probability(){
+        for (int i = 0; i < splitCnt; i++) {
+            
+        }
+        return 0;
+    }
+
+    public double bEntropy(){
+        double tmpVal = 0;
+        double res = 0;
+        for (int i = 0; i < splitCnt; i++) {
+            for (int j = 0; j < splitCnt; j++) {
+                tmpVal = (1 - rMetric( data[i] , data[j], data.length)) * getP( j);
+            }
+            res += getP(i) * Math.log(tmpVal);
+        }
+        return res/ Math.log(epsilon);
+    }
+    private List getProbArray(ArrayList data, int i){
+        List res = new ArrayList();
+        data.stream()
+                .sorted()
+                .forEach(el-> System.out.println(el));
+        return res;
+    }
+
+    private double getP( int i){return 0;}
+
+    private double rMetric(double rCur, double rPrev, double size){
         return Math.abs(rCur-rPrev)/Math.abs(size);
     }
-    private static double probability(double[] data, int a){
-        return 0;
-    }
 
-    public static double bEntropy(double[] data, int splitCnt){
-        for (int i = 0; i < data.length; i++) {
-            for (int j = 0; j < data.length; j++) {
-                final double v = 1 - probability(data, 100);
-            }
-        }
-        return 0;
-    }
-    private double getMinMaxIDs(double[] values){
-        int maxID = 0;
-        int minID = 0;
-        for (int i = 0; i < values.length; i++) {
-            if (values[i] > values[maxID]){
-                maxID = i;
-            } else if(values[i] < values[minID]){
-                minID = i;
-            }
-        }
-        return 0;
-    }
-    private double getMinimum(double[] values) {
-        if (values != null && values.length >= 1) {
+    private double getMinimum() {
+        if (data != null && data.length >= 1) {
             double min = 1.7976931348623157E308D;
 
-            for(int i = 0; i < values.length; ++i) {
-                if (values[i] < min) {
-                    min = values[i];
+            for(int i = 0; i < data.length; ++i) {
+                if (data[i] < min) {
+                    min = data[i];
                 }
             }
-
             return min;
         } else {
-            throw new IllegalArgumentException("Null or zero length 'values' argument.");
+            throw new IllegalArgumentException("Null or zero length 'data' argument.");
         }
     }
 
-    private double getMaximum(double[] values) {
-        if (values != null && values.length >= 1) {
+    private double getMaximum() {
+        if (data != null && data.length >= 1) {
             double max = -1.7976931348623157E308D;
 
-            for(int i = 0; i < values.length; ++i) {
-                if (values[i] > max) {
-                    max = values[i];
+            for(int i = 0; i < data.length; ++i) {
+                if (data[i] > max) {
+                    max = data[i];
                 }
             }
-
             return max;
         } else {
-            throw new IllegalArgumentException("Null or zero length 'values' argument.");
+            throw new IllegalArgumentException("Null or zero length 'data' argument.");
         }
     }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,18 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultIntervalCategoryDataset;
+import org.jfree.data.category.IntervalCategoryDataset;
 import org.jfree.data.statistics.HistogramDataset;
+import org.jfree.data.statistics.HistogramType;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.Random;
+
+import static org.jfree.data.statistics.HistogramType.RELATIVE_FREQUENCY;
+import static org.jfree.data.statistics.HistogramType.SCALE_AREA_TO_1;
 
 public class Main {
 
@@ -18,9 +25,11 @@ public class Main {
         Random r = new Random();
         for (int i = 0; i < 1000; i++) {
             data[i] = r.nextGaussian();
-            System.out.println(data[i]);
+//            System.out.println(data[i]);
         }
         HistogramDataset dataset = new HistogramDataset();
+//        HistogramType hType =  RELATIVE_FREQUENCYSCALE_AREA_TO_1;
+        dataset.setType(RELATIVE_FREQUENCY);
         dataset.addSeries("series label",data,SPLITCNT);
         JFreeChart chart = ChartFactory.
                 createHistogram( "plotTitle", "xaxis label", "yaxis label",
